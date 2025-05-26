0255fa9fdf45495ced8ac842f4077e64c70c782e
Maxime Herbaut <maximehrbt@gmail.com>
2025-05-26 12:33:23 +0200

    add umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
-// pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Footer from '../components/sections/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#181b1f] via-[#22272a] to-[#191b1f] text-gray-100 font-sans transition-colors duration-300">
+      <Head>
+        <script
+          async
+          defer
+          src={process.env.NEXT_PUBLIC_UMAMI_URL}
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID} 
+        />
+      </Head>
       <main className="flex-grow">
         <Component {...pageProps} />
       </main>
